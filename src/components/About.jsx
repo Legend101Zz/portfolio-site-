7b73acae7e635e3b0cfb80d90f233dddcc0387e2
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-03-18 22:22:30 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import Tilt from "react-parallax-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className="xs:w-[250px] w-full">
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
+        <img
+          src={icon}
+          alt="web-development"
+          className="w-16 h-16 object-contain"
+        />
+
+        <h3 className="text-white text-[20px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        Hi, I'm Mrigesh Thakur, a tech enthusiast with a passion for developing
+        applications in the MERN stack. Over the past 2 years, I've gained
+        experience in this field and am eager to take on new challenges. I'm
+        currently a 2nd year student pursuing a degree in Mathematics and
+        Scientific Computing at NIT Hamirpur. I've had the opportunity to work
+        with several startups in the past and am now excited to contribute to
+        the open source community. Recently, I've been exploring the world of
+        metaverse projects, where together with my friends, we created
+        functional projects for various hackathons. I'm eager to continue
+        exploring the limitless possibilities of the metaverse and make a
+        difference in the open source community and know many new things . With
+        my technical expertise, enthusiasm, and willingness to learn, I believe
+        I can be a valuable addition to any team or project.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
